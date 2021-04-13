c51e4b68802728414e8ebb606dfccfa2f605e0c2
unknown <alejandroaigner1999@hotmail.com>
2021-04-12 22:47:25 -0400

    Mejora de lo íconos del footer

--- js/app.js
+++ js/app.js
@@ -1,10 +1,1 @@
 'use strict';
-
-// const spanes = document.querySelectorAll('span.card-title');
-
-
-// // Hacer esto una función...
-// spanes.forEach((e)=>{
-// 	// e.classList += ' right-align';
-// 	console.log(e);
-// });
